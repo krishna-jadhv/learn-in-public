927cf03c16bb539a1031332d7e3afd0aa173d3de
krishna <krishnakj3312@gmail.com>
2024-10-13 22:20:31 +0530

    Random background color project

---
+++ Random-background-color/script.js
@@ -0,0 +1,34 @@
+
+
+
+const randomColors= function(){
+    const hex = "0123456789ABCDEF";
+    let color='#';
+
+    for(let i=0; i<6; i++){
+       color+=hex[(Math.floor(Math.random()*16))]
+    }
+    return color;
+}
+let intervalId;
+const StartChangingColor= function () {
+    if(!intervalId){
+        intervalId= setInterval(changeBgColor, 1000)
+    }
+
+    function changeBgColor() {
+        document.body.style.backgroundColor=randomColors();
+    }
+
+    document.querySelector('h3').innerHTML="Click STOP Button to Stop";
+    
+}
+
+const StopChangingColor= function () {
+    clearInterval(intervalId);
+    document.querySelector('h3').innerHTML="Click Start Button to Start";
+    intervalId=null;
+}
+
+document.querySelector('.start').addEventListener('click', StartChangingColor)
+document.querySelector('.stop').addEventListener('click', StopChangingColor)
